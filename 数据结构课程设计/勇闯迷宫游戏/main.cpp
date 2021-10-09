75e07df447aed139336d7e7151654b08a6c8d190
LIUC2002 <83438334+LIUC2002@users.noreply.github.com>
2021-10-09 18:47:05 +0800

    勇闯迷宫游戏初稿（使用stl库vector）
    此版本仍用了stl的vector库，正准备尝试自己写一个Vector

---
+++ 数据结构课程设计/勇闯迷宫游戏/main.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<vector>
+#include"Vector.hpp"
+
+using namespace std;
+
+const int maxn = 100, maxm = 100;
+int n, m, minLength = maxn * maxm, step[4][2] = { {1,0},{0,1},{-1,0},{0,-1} };
+bool map[maxn + 1][maxm + 1], b[maxn + 1][maxm + 1];
+
+//定义点类
+class Point {
+public:
+	int _x, _y;
+	Point(int x = 0, int y = 0) :_x(x), _y(y) {}
+}startPoint, endPoint;
+
+//定义路线和最短路线
+Vector<Point>route, shortest_route;
+vector<Point>p;
+
+//输入函数
+void input() {
+	//起点(1,1);终点(n,m)
+	cin >> n >> m;
+	startPoint = Point(1, 1);
+	endPoint = Point(n, m);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> map[i][j];
+		}
+	}
+}
+
+//输出函数
+void output() {
+	VectorNode<Point>* vectorNode = shortest_route.getHead()->_next;
+	cout << "(" << vectorNode->_data._x << "," << vectorNode->_data._y << ")";
+	vectorNode = vectorNode->_next;
+	for (; vectorNode;vectorNode = vectorNode->_next) {
+		cout << "--->(" << vectorNode->_data._x << "," << vectorNode->_data._y << ")";
+	}
+}
+void search_shortest_route(int x, int y) {
+	if (route.getLength() >= minLength) {
+		return;
+	}
+	else if (x == endPoint._x && y == endPoint._y) {
+		shortest_route = route;
+		minLength = route.getLength();
+		return;
+	}
+	int tx, ty;
+	for (int i = 0; i < 4; i++) {
+		tx = x + step[i][0];
+		ty = y + step[i][1];
+		if (tx >= 1 && ty >= 1 && tx <= n && ty <= m && !map[tx][ty] && !b[tx][ty]) {
+			b[tx][ty] = 1;
+			route.PushBack(Point(tx, ty));
+			search_shortest_route(tx, ty);
+			route.PopBack();
+			b[tx][ty] = 0;
+		}
+
+	}
+}
+int main() {
+	cout << "请输入迷宫的行数和列数，然后换行再输入01迷宫，1表示有障碍" << endl
+		<< "（若老师有兴趣检查我的代码正确性，又没有兴趣自己构建一个迷宫，" << endl
+		<< "我在代码最后贴了几个较为复杂的输入，可以直接复制粘贴）" << endl;
+	input();
+	route.PushBack(startPoint);
+	search_shortest_route(1, 1);
+	route.PopBack();
+	output();
+	return 0;
+}
+/*
+* 输入1
+8 9
+0 1 1 1 1 0 0 0 0
+0 0 0 0 0 0 1 1 0
+1 0 1 1 1 0 0 0 0
+0 0 0 0 1 1 0 1 0
+0 1 1 0 0 0 0 1 0
+0 1 0 0 1 0 1 0 0
+1 0 1 0 1 0 1 0 0
+0 0 0 0 0 0 1 0 0
+ * 输入2
+
+*/
